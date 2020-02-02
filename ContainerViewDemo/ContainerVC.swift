af1466fc10280fff02ebb71bcc0aa8364207b2cc
Usaid Ather <usaid.ather@gmail.com>
2020-02-02 13:14:34 +0500

    first commit

---
+++ ContainerViewDemo/ContainerVC.swift
@@ -0,0 +1,13 @@
+//
+//  ContainerVC.swift
+//  ContainerViewDemo
+//
+//  Created by Usaid Ather on 01/02/2020.
+//  Copyright © 2020 Usaid Ather. All rights reserved.
+//
+
+import UIKit
+
+class ContainerVC: NSObject {
+
+}
---
+++ ContainerViewDemo/ViewControllerA.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerA.swift
+//  ContainerViewDemo
+//
+//  Created by Usaid Ather on 01/02/2020.
+//  Copyright © 2020 Usaid Ather. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerA: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
